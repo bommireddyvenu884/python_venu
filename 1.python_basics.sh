2cbbf8b3a9bac0ab95eda56b415cfe7ecd98b8f0
Venu <bommireddy2000@gmail.com>
2025-07-04 14:55:02 +0530

    python new file

---
+++ 1.python_basics.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# print 1 to 10 similar to shell script.
+for I in range(10):
+print(I)
+
+# working with strings.
+quote1 ="stop thinking start doing"
+quote2 ="stop thinking start doing"
+quote3 ="stop thinking start doing"
+quote4 = ''' hello welcome to the devops python'''
+
+print(quote1)
+print(quote2)
+print(quote3)
+print(quote4)
+
