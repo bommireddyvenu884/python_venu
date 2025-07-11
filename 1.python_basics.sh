07d90fc27536a477714a80c65e94fd465ce0085f
Venu <bommireddy2000@gmail.com>
2025-07-11 11:34:30 +0530

    updste the python code

--- 1.python_basics.sh
+++ 1.python_basics.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # print 1 to 10 similar to shell script.
 for I in range(10):
-   print(I)
+    print(I)
 
-# working with strings.
+# Working with strings
 quote1 = "stop thinking start doing"
 quote2 = "stop thinking start doing"
 quote3 = "stop thinking start doing"
-quote4 =  ''' hello welcome to the devops python'''
+quote4 = '''hello welcome to the devops python'''
 
 print(quote1)
 print(quote2)
 print(quote3)
 print(quote4)
-print(quote5)
