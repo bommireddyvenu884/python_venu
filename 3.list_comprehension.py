ecaed5f8ff728c3c39e954d88be0c6309038bd5b
Venu <bommireddy2000@gmail.com>
2025-07-12 15:53:09 +0530

    saved local change

--- 3.list_comprehension.py
+++ 3.list_comprehension.py
@@ -1,9 +1,12 @@
 even_numbers = []
 odd_numbers = []
-for num in range(2,100):
-if num%2 == 0:
-even_numbers.append(num)
-else:
-odd_numbers.append(num)
-print(even_numbers)
-print(odd_numbers)
+
+for num in range(2, 100):
+    if num % 2 == 0:
+        even_numbers.append(num)
+    else:
+        odd_numbers.append(num)
+
+print("Even numbers:", even_numbers)
+print("Odd numbers:", odd_numbers)
+
